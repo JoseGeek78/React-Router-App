578d2914af27f13667491d789e59538d43f69c24
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-26 20:21:53 +0100

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
+    element: <div>Hello world!</div>,
   },
 ]);
 
